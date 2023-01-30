60c5e6697801558519d8d6d1ffdfeb3bcc473402
Iskren <iskren.dev@gmail.com>
2023-01-30 22:05:15 +0100

    feat: handleMove logic change and styles

--- src/pages/sandpack.tsx
+++ src/pages/sandpack.tsx
@@ -1,7 +1,8 @@
+import Image from 'next/image';
 import { useContext, useEffect, useState } from "react";
 import { GithubDataContext } from '../context/github-context';
 import { styled } from '@mui/material/styles';
-import { Paper, Grid, Box } from '@mui/material';
+import { Paper, Grid, Box, Button } from '@mui/material';
 import SandpackHeader from '../components/sandpack-hreader';
 
 type BranchCategory = {
@@ -13,7 +14,6 @@ export default function SandpackPage() {
 	const { branches } = useContext(GithubDataContext);
 
 	// TODO: Abstract this into GithubDataContext.
-	// Initial mocking strategy for moved branches.
 
 	const [localBranches, setLocalBranches] = useState<BranchCategory[]>([
 		{ name: "In progress", branches: [...branches] },
@@ -24,11 +24,12 @@ export default function SandpackPage() {
 	const handleMove = (branch: string, from: string, to: string) => {
 		// Map over the existing branches to create a new array with updated values
 		const updatedBranches = localBranches.map((category) => {
+
 			// If the current category is the "from" category, remove the branch from the branches array
 			if (category.name === from) {
 				return {
 					...category,
-					branches: category.branches.filter((branchName) => branchName !== branch),
+					branches: category.branches.filter((branchName: any) => branchName.name !== branch), //TODO: Type Data
 				};
 			}
 			// If the current category is the "to" category, add the branch to the branches array
@@ -38,16 +39,12 @@ export default function SandpackPage() {
 					branches: [...category.branches, branch],
 				};
 			}
-
 			return category;
 		});
 
+
 		setLocalBranches(updatedBranches);
 	};
-
-	useEffect(() => {
-		console.log(localBranches);
-	}, [branches, localBranches]);
 
 	return (
 		<Box sx={{ margin: '4%' }}>
@@ -55,13 +52,13 @@ export default function SandpackPage() {
 				<SandpackHeader />
 
 				{/* In Progress */}
-				<Grid item>
+				<Grid item >
 					<p>In Progress ({localBranches[0].branches.length})</p>
 					{localBranches[0].branches.map((branch: any, i: number) => (
-						<>
+						<Box key={i} sx={{ position: 'relative' }}>
 							<SItem key={i} elevation={0}>{branch.name}</SItem>
-							<button onClick={() => handleMove('New-Index-Page', "In progress", "Review")}>Move</button>
-						</>
+							<SRightIcon src='/images/move-right.svg' alt="Right arrow" width={18} height={18} onClick={() => handleMove(branch.name, "In Progress", "Review")} />
+						</Box>
 					))}
 				</Grid>
 
@@ -69,10 +66,15 @@ export default function SandpackPage() {
 				<Grid item>
 					<p>Review ({localBranches[1].branches.length})</p>
 					{localBranches[1].branches.map((branch: any, i: number) => (
-						<>
-							<SItem key={i} elevation={0}>{branch.name}</SItem>
-							<button onClick={() => handleMove('New-Index-Page', "Review", "Ready to merge")}>Move</button>
-						</>
+						// <Box key={i} sx={{
+						// 	// position: 'relative'
+						// }}>
+						<SBox key={i}>
+							<SItem elevation={0}>{branch}</SItem>
+							<SLeftIcon src='/images/move-left.svg' alt="Left arrow" width={18} height={18} onClick={() => handleMove(branch.name, "Review", "In Progress")} />
+							<SRightIcon src='/images/move-right.svg' alt="Right arrow" width={18} height={18} onClick={() => handleMove(branch.name, "Review", "In Progress")} />
+						</SBox>
+						// </Box>
 					))}
 				</Grid>
 
@@ -82,22 +84,42 @@ export default function SandpackPage() {
 					{localBranches[2].branches.map((branch: any, i: number) => (
 						<>
 							<SItem key={i} elevation={0}>{branch.name}</SItem>
-							<button onClick={() => handleMove('New-Index-Page', "Review", "Ready to merge")}>Move</button>
+							{/* <button onClick={() => handleMove(branch.name, "Ready to merge", "In Progress")}>Move</button> */}
 						</>
 					))}
 				</Grid>
 			</Grid>
-		</Box>
+		</Box >
 	)
 }
 
-// Translate in rem 367px
+
+const SBox = styled(Box)(({ }) => ({
+	position: 'relative',
+}));
+
+const SLeftIcon = styled(Image)(({ }) => ({
+	position: 'absolute',
+	top: 22,
+	left: 25,
+}));
+
+const SRightIcon = styled(Image)(({ }) => ({
+	position: 'absolute',
+	top: 22,
+	right: 30,
+}));
+
 const SItem = styled(Paper)(({ theme }) => ({
 	...theme.typography.h3,
-	margin: theme.spacing(1),
+	margin: theme.spacing(2),
 	width: 400,
 	textAlign: 'center',
 	backgroundColor: theme.palette.primary.main,
 	height: 60,
 	lineHeight: '60px',
+	'&:hover': {
+		cursor: 'pointer',
+		backgroundColor: '#383838',
+	}
 }));
