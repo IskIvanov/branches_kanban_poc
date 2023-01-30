e71465dbcf07e9522c957879e41655e1f5c5bbdf
Iskren <iskren.dev@gmail.com>
2023-01-30 17:16:18 +0100

    feat: Initial mocking strategy for movable branches

--- src/pages/sandpack.tsx
+++ src/pages/sandpack.tsx
@@ -1,28 +1,65 @@
-import Image from 'next/image';
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import { GithubDataContext } from '../context/github-context';
 import { styled } from '@mui/material/styles';
-import { Paper, Grid } from '@mui/material';
+import { Paper, Grid, Box } from '@mui/material';
+import SandpackHeader from '../components/sandpack-hreader';
 
-// TODO: Populate component with kanban branches
+type BranchCategory = {
+	name: string;
+	branches: string[];
+}
 
-export default function Sandpack() {
-	const { branches, stars, setBranches, setStars } = useContext(GithubDataContext);
+export default function SandpackPage() {
+	const { branches } = useContext(GithubDataContext);
+
+	// TODO: Abstract this into GithubDataContext.
+	// Initial mocking strategy for moved branches.
+
+	const [localBranches, setLocalBranches] = useState<BranchCategory[]>([
+		{ name: "In progress", branches: [...branches] },
+		{ name: "Review", branches: [...branches] },
+		{ name: "Ready to Merge", branches: [...branches] },
+	]);
+
+
+	useEffect(() => {
+		console.log(localBranches);
+	}, [branches]);
 
 	return (
-		<Grid container spacing={2}>
-			<Grid item>
-				<span><Image src='/images/star.svg' alt='Star' width={20} height={20} className='testingStles relative bg-slate-400' /> {stars}</span>
+		<Box sx={{ margin: '4%' }}>
+			<Grid container spacing={4} direction="row" justifyContent="center">
+				<SandpackHeader />
+
+				{/* In Progress */}
+				<Grid item>
+					<p>In Progress ({localBranches[0].branches.length})</p>
+					{localBranches[0].branches.map((branch: any, i: number) => (
+						<SItem key={i} elevation={0}>{branch.name}</SItem>
+					))}
+				</Grid>
+
+				{/* Review */}
+				<Grid item>
+					<p>Review ({localBranches[0].branches.length})</p>
+					{localBranches[1].branches.map((branch: any, i: number) => (
+						<SItem key={i} elevation={0}>{branch.name}</SItem>
+					))}
+				</Grid>
+
+				{/* Ready to merge */}
+				<Grid item>
+					<p>Ready to merge ({localBranches[0].branches.length})</p>
+					{localBranches[2].branches.map((branch: any, i: number) => (
+						<SItem key={i} elevation={0}>{branch.name}</SItem>
+					))}
+				</Grid>
 			</Grid>
-			<Grid item>
-				{branches.map((branch: any, i: number) => (
-					<SItem key={i} elevation={0}>{branch.name}</SItem>
-				))}
-			</Grid>
-		</Grid>
+		</Box>
 	)
 }
 
+// Translate in rem 367px
 const SItem = styled(Paper)(({ theme }) => ({
 	...theme.typography.h3,
 	margin: theme.spacing(1),
